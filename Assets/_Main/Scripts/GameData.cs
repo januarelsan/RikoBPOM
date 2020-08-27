5d2904fed56ed77bb688db3a2d2800f827586565
januarelsan <januarelsan@gmail.com>
2020-08-27 14:42:18 +0700

    Initial Project

---
+++ Assets/_Main/Scripts/GameData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : Singleton<GameData> {
+
+	[SerializeField]
+	private bool resetData;
+
+	// Use this for initialization
+	void Start () {
+		if(resetData)
+			ResetData();
+	}
+
+	void ResetData(){
+		if(resetData)
+			PlayerPrefs.DeleteAll();
+	}
+
+	public int AudioOn
+	{
+		get { return PlayerPrefs.GetInt("AudioOn",1);}
+		set { PlayerPrefs.SetInt("AudioOn",value);}
+	}
+
+		
+	
+}
---
+++ Assets/_Main/Scripts/Jumping.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumping : MonoBehaviour
+{
+    public float playerUpwardForce;
+
+    private Rigidbody2D rb2;
+    private bool jumping;
+
+    void Awake(){
+        rb2 = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Jump(){
+        if(!jumping){
+            rb2.AddForce(new Vector3(0,playerUpwardForce,0), ForceMode2D.Impulse);
+            GetComponent<Animator>().SetTrigger("Jump");
+            jumping = true;
+        }
+    }
+
+    
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Ground"){
+            jumping = false;
+        }
+    }
+}
