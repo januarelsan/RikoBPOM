7b9960c3ba73926e38be3da11755e774a2d3babc
januarelsan <januarelsan@gmail.com>
2020-08-28 16:58:47 +0700

    Install PlayMaker

---
+++ Assets/_Main/Scripts/MainMusic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMusic : MonoBehaviour
+{
+    private static MainMusic playerInstance;
+    void Awake(){
+        DontDestroyOnLoad (this);
+            
+        if (playerInstance == null) {
+            playerInstance = this;
+        } else {
+            DestroyObject(gameObject);
+        }
+    }
+
+    void Update(){
+        
+        // Debug.Log("Music is: " + GameData.Instance.AudioOn);
+        
+        if(GameData.Instance.AudioOn == 0){
+            if(GetComponent<AudioSource>().isPlaying)
+                GetComponent<AudioSource>().Pause();
+        } else {
+            if(!GetComponent<AudioSource>().isPlaying)
+                GetComponent<AudioSource>().Play();
+        }
+        
+
+        
+    }
+    
+    
+}
