9418feaab3599bd770e9b8f5139b8c3dbfa20b9d
januarelsan <januarelsan@gmail.com>
2020-09-04 13:59:31 +0700

    Import Epic Fx

---
+++ Assets/_Main/Scripts/PlayerCollectingItem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollectingItem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ApplyEffect(int itemType){
+        
+        switch (itemType)
+        {
+            case 0:
+                Debug.Log("Get Masker");
+                GetComponent<PlayerHealth>().AddMask();
+                break;
+            case 1:
+                Debug.Log("Get Hit");
+                GetComponent<PlayerHealth>().SubHealth(20);
+                break;
+            default:
+                Debug.Log("Zero");
+                break;
+        }        
+
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.tag == "CollectibleItem")   {
+            ApplyEffect(col.gameObject.GetComponent<CollectibleItem>().GetType());
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ Assets/_Main/Scripts/PlayerHealth.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private Image maskerOnHealtImage;
+
+    private float health = 100;
+    private int masked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthSlider.value = health;
+
+        if(masked > 0){
+            maskerOnHealtImage.enabled = true;
+        } else {
+            maskerOnHealtImage.enabled = false;
+        }
+    }
+
+    public void AddHealth(float value){        
+        health += value;
+
+        if(health > 100)
+            health = 100;
+    }
+
+    public void SubHealth(float value){
+
+        if(masked > 0){
+            health -= value/2;    
+        }else{
+            health -= value;
+            masked--;
+        }
+        
+        if(health <= 0)
+        {
+
+        }
+    }
+
+    public void AddMask(){
+        
+        masked = 3;
+        
+    }
+
+    public void SubMask(){
+        masked--;        
+    }
+
+
+
+}
